6eb6dfb3f04ceda5fea326436a9157acd74f8af9
Ricardo Blanco <149442545+RicardoBlancoRBT@users.noreply.github.com>
2024-07-08 22:38:34 -0600

    feat: searching and styles

--- src/VichosWorldApp.tsx
+++ src/VichosWorldApp.tsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react"
 import { vichosWorldProductsRepositoryApi } from "./repositories/VichosWorld-Products-Interface/VichosWorldProductsRepositoryApi"
 import { Product } from "./domain/entities/products"
 import { ProductCard } from "./infrastructure/presentation/components/ProductCard"
-
+import logo from './assets/vichosworld.png';
 
 export const VichosWorldApp = () => {
     let getProducts= true
+    const [isLoading, setIsLoading] = useState(false);
 
     const [productToSearch, setProductToSearch] = useState('')
     const [products, setProducts] = useState<Product[]>([])
@@ -13,13 +14,19 @@ export const VichosWorldApp = () => {
 
     const [productsToShow, setProductsToShow] = useState<Product[]>([])
 
+    const phoneNumber = '+525574462403'; // Reemplaza con el número de teléfono correcto
+    const message = 'Me interesa algunos productos que he visto en Vicho´s World'; // Mensaje predefinido
+    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+
     useEffect(() => {
         if (!getProducts) {
             return
         }
         const fetchProducts = async () => {
+            setIsLoading(true);
             const result = await vichosWorldProductsRepositoryApi.getProductsStock()
             if (result != null) {
+                setIsLoading(false);
                 console.log('result fetched', result)
                 result.sort((a, b) => a.name.localeCompare(b.name));
                 const availableProducts = result.filter(product => product.isActive);
@@ -34,21 +41,29 @@ export const VichosWorldApp = () => {
     }, [])
 
     const findByName = (value: string) => {
-        return products.filter((product) =>
-            product.name.includes(value),
+        const finded = products.filter((product) =>
+            product.name.includes(value.toUpperCase()),
         );
+        console.log('finded findByName', finded)
+        return finded ?? products
 
     }
     const findByBarCode = (value: string) => {
-        return products.filter((product) =>
-            product.barCode?.includes(value),
+        const finded = products.filter((product) =>
+            product.barCode?.includes(value.toUpperCase()),
         );
+        console.log('finded findByBarCode', finded)
+        return finded ?? products
     }
 
     return (
         <>
-            <h1>Vicho's World</h1>
-            <div style={{ display: 'flex', justifyContent: 'space-between', width: '200px' }}>
+        <a href={whatsappUrl} target="_blank" rel="noopener noreferrer">
+
+        <img src={logo} alt="Logo de la aplicación" className="app-logo" />
+        </a>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', marginBottom:'8px', marginTop:'16px' }}>
+            <div style={{ display: 'flex', justifyContent: 'space-between', width: '200px', marginBottom:'8px' }}>
                 <div>
                     <input
                         type="radio"
@@ -70,6 +85,7 @@ export const VichosWorldApp = () => {
             </div>
 
             <input
+            style={{ marginBottom: '16px', marginTop: '16px', marginRight:'16px', width: '300px'}}
                 type="text"
                 value={productToSearch}
                 onChange={(e) => {
@@ -82,18 +98,42 @@ export const VichosWorldApp = () => {
                     }
 
                 }}
-                placeholder="Nombre"
+                placeholder={`Buscar por ${searchType === 'name' ? 'nombre' : 'código de barras'}`}
             />
 
-            <button onClick={() => {
+            <button 
+            style={{
+                backgroundColor: '#b5d86d', // Color de fondo
+                color: 'white', // Color del texto
+                border: 'none', // Sin borde
+                padding: '10px 20px', // Relleno
+                textAlign: 'center', // Alineación del texto
+                textDecoration: 'none', // Sin decoración de texto
+                display: 'inline-block', // Display
+                fontSize: '16px', // Tamaño de la fuente
+                margin: '4px 2px', // Margen
+                cursor: 'pointer', // Cursor
+                borderRadius: '4px' // Radio del borde
+              }}
+            
+            onClick={() => {
+                setProductToSearch('') 
                 setProductsToShow(products)
-            }}>Limpiar Busqueda</button>
+            }}
+            
+            >Limpiar Busqueda</button>
+            </div>
+            <div>
+            {isLoading ? (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', marginBottom:'8px', marginTop:'16px' }}><h1 className="loading">Cargando ...</h1></div> 
+    ) :(
+           
             <div className="card-grid">
                 {productsToShow.map((product) => (
                     <ProductCard key={product.productId} product={product} />
                 ))}
-            </div>
-
+            </div> )}
+ </div>
         </>
     )
 }
--- src/infrastructure/presentation/components/ProductCard.tsx
+++ src/infrastructure/presentation/components/ProductCard.tsx
@@ -2,26 +2,27 @@ import { Product } from "../../../domain/entities/products";
 import '../../../ProductCard.css';
 interface Props {
     product: Product;
-  }
-  
-  export const ProductCard = ({ product }: Props) => {
+}
+
+export const ProductCard = ({ product }: Props) => {
+    const discountPrice = product.discountPrice ?? 0.00
     return (
-      <div className="product-card">
-        <img
-          src={product.images?.mainImage || '/path/to/default/image.png'}
-          alt={product.name}
-          className="product-image"
-        />
-        <div className="product-info">
-          <h2 className="product-name">{product.name}</h2>
-          <h2 className="product-barcode">{product.barCode}</h2>
-          <p className="product-description">{product.description}</p>
-          <p className="product-category">{product.category}</p>
-          <p className="product-brands">{product.brands}</p>
-          <p className="product-price">{`$${product.price}`}</p>
-          <p className="product-discountPrice">{`$${product.discountPrice}`}</p>
-          <p className="product-stock">{`Stock: ${product.stock}`}</p>
+        <div className="product-card">
+            <h2 className="product-name">{product.barCode}</h2>
+            <img
+                src={product.images?.mainImage || '/path/to/default/image.png'}
+                alt={product.name}
+                className="product-image"
+            />
+            <div className="product-info">
+                <h4 className="product-name">{product.name}</h4>
+
+                <p className="product-category">{`Categoria: ${product.category}`}</p>
+                <p className="product-brands">{`Marca: ${product.brands}`}</p>
+                <p className="product-price">{`Precio: $${product.price}`}</p>
+                <p className="product-discountPrice">{discountPrice > 0 ? `Descuento: $${discountPrice}` : ``}</p>
+                <p className="product-stock">{`Stock: ${product.stock}`}</p>
+            </div>
         </div>
-      </div>
     );
-  };
+};
